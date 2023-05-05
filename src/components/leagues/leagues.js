b843f04b5065b78e9d5df36b14f89e4e02e6b914
Bliss <blissmaake140@gmail.com>
2023-05-05 13:32:17 +0300

    Adding the leagues feature

---
+++ src/components/leagues/leagues.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Tabletr from "../tabletr"
+
+export default function League({ fixture }) {
+  return (
+    <div>
+      <div className="p-2 text-white bg-gray-700">{fixture[0].league.name}</div>
+      {fixture.map((fixture) => (
+        <Tabletr fixture={fixture} />
+      ))}
+      <div className="border-solid border-2 border-black"></div>
+    </div>
+  )
+}
